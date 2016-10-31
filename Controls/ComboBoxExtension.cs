3973f626a720586f228e1d570e99327cf8ca3d03
ainwood <cfcgotm@gmail.com>
2016-10-31 22:43:10 +1300

    Basic Framework for reading and displaying data
    Haven't used GitHub before, so hope I've got it right.  Think I've provided all files, but let me know if you get errors.
    
    Not convinced this (reflection) is the best way to go, or whether we should work with classes directly.  Had a few issues, including needing to find some way of getting the class names for the virtual linked classes in each class.

---
+++ Controls/ComboBoxExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CivGen.Controls
+{
+
+    public class ComboboxItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+
+    public static class ComboBoxExtension
+    {
+
+    }
+}
